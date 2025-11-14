f0217b2a7bbe4d66ba9c0cd7fe1089173d551757
MUGESH SELVARAJ <mugeshs.22cse@ongu.edu>
2025-11-14 14:16:47 +0530

    Executed sevaral string functions

---
+++ String_functions/string.js
@@ -0,0 +1,22 @@
+let word = "Hello World!"
+
+//index() - used to find the index of the given character
+console.log(word.indexOf('H'))
+
+//at() - allows both negative and positive index
+console.log(word.at(-1))
+
+//startsWith() - used to find whether the given string is in the starting
+console.log(word.startsWith("Hel"))
+
+//endsWith() - same as startswith, but backwards
+console.log(word.endsWith("!"))
+
+//slice() - used to trim the string from beginning index or in betweeen start and end
+console.log(word.slice(2, 4))
+
+//toUpperCase() - used to change the case to upper case
+console.log(word.toUpperCase())
+
+//trim() - used to trim the spaces from both front and beginning
+console.log(word.trim())
